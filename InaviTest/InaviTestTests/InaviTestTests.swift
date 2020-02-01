45c6d4c18365c52f2cdbb82390b374c72fcae1fe
onlyhjh <60452571+onlyhjh@users.noreply.github.com>
2020-02-01 09:59:07 +0900

    new inavi test project
    create new project

---
+++ InaviTest/InaviTestTests/InaviTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  InaviTestTests.swift
+//  InaviTestTests
+//
+//  Created by Home on 2020/02/01.
+//  Copyright © 2020 Inavi. All rights reserved.
+//
+
+import XCTest
+@testable import InaviTest
+
+class InaviTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
